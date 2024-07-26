e1cbfee0fd177fbccd39637df0b5ede79e777e79
Ed Chen <ed@granden.rocks>
2024-07-26 12:40:17 +0800

    [Add]加上UI在地化處理 Demo

---
+++ Assets/Scenes/UILocalizationDemo/Scripts/LocalizationDemoUI.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using XPlan.UI;
+
+namespace XPlan.Demo.Localization
+{
+	public class LocalizationDemoUI : UIBase
+	{
+	}
+}
