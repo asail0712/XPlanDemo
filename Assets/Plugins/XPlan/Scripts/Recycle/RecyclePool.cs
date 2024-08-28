a706a84e657286941b44ef2896de0d0da782616f
Ed Chen <ed@granden.rocks>
2024-08-28 10:12:51 +0800

    [Modify]優化RecyclePool流程

--- Assets/Plugins/XPlan/Scripts/Recycle/RecyclePool.cs
+++ Assets/Plugins/XPlan/Scripts/Recycle/RecyclePool.cs
@@ -7,62 +7,16 @@ using XPlan.DebugMode;
 
 namespace XPlan.Recycle
 { 
-    public class PoolInfo<T> where T: IPoolable, new()
+    public static class RecyclePool<T> where T : IPoolable, new()
     {
-        private Queue<T> poolableQueue;
-        private GameObject prefab;
-        private int totalNum;
-             
-        public PoolInfo()
-		{
-            poolableQueue   = new Queue<T>();
-            totalNum        = 0;
-        }
+        static private Queue<T> poolableQueue   = new Queue<T>();
+        static private int totalNum             = 0;
+        static private GameObject prefabInstance;
 
-        public void AddPrefab(GameObject prefab, int num)        
-		{
-            if (!typeof(PoolableComponent).IsAssignableFrom(typeof(T)))
-            {
-                return;
-            }
-
-            PoolableComponent dummy = null;
-
-            if (!prefab.TryGetComponent<PoolableComponent>(out dummy))
-			{
-                return;
-			}
-
-            // 考慮到是monobehavior，生成方式會不一樣
-            this.prefab     = prefab;
-            this.totalNum   = num;
-
-            for (int i = 0; i < num; ++i)
-            {
-                GameObject go   = GameObject.Instantiate(prefab);
-                T comp          = go.GetComponent<T>();
-
-                go.SetActive(false);
-                poolableQueue.Enqueue(comp);
-            }
-        }
-
-        public void AddPoolable(List<T> poolList)
-        {
-            for (int i = 0; i < poolList.Count; ++i)
-            {
-                poolableQueue.Enqueue(poolList[i]);
-            }
-
-            totalNum += poolList.Count;
-        }
-
-        public void ResetPool()
-		{
-			poolableQueue.Clear();
-		}
-
-		public T SpawnOne()
+        /**************************************************
+         * 生成流程
+         * ************************************************/
+        static public T SpawnOne()
 		{
             T poolable  = default(T);
             Type type   = typeof(T);
@@ -73,13 +27,13 @@ namespace XPlan.Recycle
             {
                 if (poolableQueue.Count == 0)
                 {
-                    if (prefab == null)
+                    if (prefabInstance == null)
                     {
                         LogSystem.Record($"backup 物件 為空，無法生成新GameObject !!", LogType.Error);
                     }
                     else
                     {
-                        GameObject go   = GameObject.Instantiate(prefab);
+                        GameObject go   = GameObject.Instantiate(prefabInstance);
                         poolable        = go.GetComponent<T>();
 
                         ++totalNum;
@@ -112,43 +66,6 @@ namespace XPlan.Recycle
             return poolable;
         }
 
-        public void Recycle(T poolable)
-		{
-            poolable.OnRecycle();
-            
-            poolableQueue.Enqueue(poolable);
-        }
-
-        public int PoolNum()
-        {
-            return poolableQueue.Count;
-        }
-
-        public int TotalNum()
-		{
-            return totalNum;
-        }
-    }
-
-    public static class RecyclePool<T> where T : IPoolable, new()
-    {
-        static private Dictionary<Type, PoolInfo<T>> poolInfoList = new Dictionary<Type, PoolInfo<T>>();
-
-        /**************************************************
-         * 生成流程
-         * ************************************************/
-        static public T SpawnOne()
-		{
-            Type type = typeof(T);
-
-            if (!poolInfoList.ContainsKey(type))
-            {
-                return default(T);
-            }
-
-            return poolInfoList[type].SpawnOne();
-        }
-
         static public List<T> SpawnList(int num)
         {
             List<T> result = new List<T>();
@@ -161,21 +78,11 @@ namespace XPlan.Recycle
             return result;
         }
 
-        static public void Recycle(T something)
+        static public void Recycle(T poolable)
 		{
-            if(something == null)
-			{
-                return;
-			}
+            poolable.OnRecycle();
 
-            Type type = typeof(T);
-
-            if (!poolInfoList.ContainsKey(type))
-            {
-                return;
-            }
-
-            poolInfoList[type].Recycle(something);
+            poolableQueue.Enqueue(poolable);
         }
 
         static public void RecycleList(List<T> goList)
@@ -190,30 +97,12 @@ namespace XPlan.Recycle
          * *************************************************/
         static public int GetTotalNum()
 		{
-            Type type = typeof(T);
-
-            if (poolInfoList.ContainsKey(type))
-            {
-                PoolInfo<T> poolInfo = poolInfoList[type];
-
-                return poolInfo.TotalNum();
-            }
-
-            return 0;
+            return totalNum;
         }
 
         static public int GetPoolNum()
         {
-            Type type = typeof(T);
-
-            if (poolInfoList.ContainsKey(type))
-            {
-                PoolInfo<T> poolInfo = poolInfoList[type];
-
-                return poolInfo.PoolNum();
-            }
-
-            return 0;
+            return poolableQueue.Count;
         }
 
         /**************************************************
@@ -221,69 +110,49 @@ namespace XPlan.Recycle
          * *************************************************/
         static public bool RegisterType(GameObject prefab, int num = 5)
         {
-            Type type               = typeof(T);
-            PoolInfo<T> poolInfo    = null;
-
-            if (poolInfoList.ContainsKey(type))
+            if (!typeof(PoolableComponent).IsAssignableFrom(typeof(T)))
             {
-                poolInfo = poolInfoList[type];
-            }
-            else
-            {
-                poolInfo = new PoolInfo<T>();
-
-                poolInfoList.Add(type, poolInfo);
+                return false;
             }
 
-            poolInfo.AddPrefab(prefab, num);
+            PoolableComponent dummy = null;
+
+            if (!prefab.TryGetComponent<PoolableComponent>(out dummy))
+            {
+                return false;
+            }
+
+            // 考慮到是monobehavior，生成方式會不一樣
+            prefabInstance  = prefab;
+            totalNum        = num;
+
+            for (int i = 0; i < num; ++i)
+            {
+                GameObject go = GameObject.Instantiate(prefab);
+                T comp = go.GetComponent<T>();
+
+                go.SetActive(false);
+                poolableQueue.Enqueue(comp);
+            }
 
             return true;
         }
 
-        static public bool RegisterType(List<T> compList)
+        static public bool RegisterType(List<T> poolList)
         {
-            Type type = typeof(T);
-            PoolInfo<T> poolInfo = null;
-
-            if (poolInfoList.ContainsKey(type))
+            for (int i = 0; i < poolList.Count; ++i)
             {
-                poolInfo = poolInfoList[type];
-                poolInfo = poolInfoList[type];
-            }
-            else
-            {
-                poolInfoList.Add(type, poolInfo);
+                poolableQueue.Enqueue(poolList[i]);
             }
 
-            poolInfo.AddPoolable(compList);
+            totalNum += poolList.Count;
 
             return true;
         }
 
         static public void UnregisterType()
         {
-            Type type = typeof(T);
-
-            if (!poolInfoList.ContainsKey(type))
-            {
-                return;
-            }
-
-            PoolInfo<T> poolInfo = poolInfoList[type];
-
-            // 清空的目的是
-            // 避免更換場景時，pool info裡面有被釋放的物件，導致null
-            poolInfo.ResetPool();
-        }
-
-        static public void UnregisterAll()
-        {
-            List<Type> keyList = new List<Type>(poolInfoList.Keys);
-
-            foreach (Type key in keyList)
-			{
-                UnregisterType();
-            }
+            poolableQueue.Clear();
         }
     }
 }
