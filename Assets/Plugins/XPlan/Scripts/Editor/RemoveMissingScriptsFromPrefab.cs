20971b342320cedd05eae20518a15c11253f0960
Ed Chen <ed@granden.rocks>
2024-11-29 11:15:53 +0800

    [Modify]工具類功能修改

--- Assets/Plugins/XPlan/Scripts/Editor/RemoveMissingScriptsFromPrefab.cs
+++ Assets/Plugins/XPlan/Scripts/Editor/RemoveMissingScriptsFromPrefab.cs
@@ -1,18 +1,12 @@
 ﻿using UnityEditor;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 namespace XPlan.Editors
 {
-    using UnityEditor;
-    using UnityEngine;
-    using UnityEngine.Rendering;
-
-    public class RemoveShadowsFromPrefab : MonoBehaviour
+    public class RemoveMissingScriptsFromPrefab : MonoBehaviour
     {
-        // 在 Unity 編輯器中創建一個菜單項
-        [MenuItem("XPlanTools/Remove Shadows from Selected Prefab")]
-        static void RemoveShadowsFromSelectedPrefab()
+        [MenuItem("XPlanTools/Remove Missing Scripts From Prefab")]
+        private static void RemoveMissingScriptsFromSelectedPrefab()
         {
             // 獲取當前選中的 GameObject
             GameObject selectedObject = Selection.activeGameObject;
@@ -40,29 +34,16 @@ namespace XPlan.Editors
             }
 
             // 遍歷 Prefab 中的所有子物件
-            Renderer[] renderers = prefabInstance.GetComponentsInChildren<Renderer>(true);
+            GameObject[] childGOList = prefabInstance.GetComponentsInChildren<GameObject>(true);
 
-            // 記錄修改，允許撤銷操作
-            Undo.RecordObject(prefabInstance, "Remove Shadows from Prefab");
-
-            foreach (Renderer renderer in renderers)
+            foreach (GameObject childGO in childGOList)
             {
-                // 禁用投影陰影
-                renderer.shadowCastingMode = ShadowCastingMode.Off;
-
-                // 禁用接收陰影
-                renderer.receiveShadows = false;
-
-                // 標記 Renderer 為已修改，準備保存
-                EditorUtility.SetDirty(renderer);
+                GameObjectUtility.RemoveMonoBehavioursWithMissingScript(childGO);
             }
 
             // 保存 Prefab 修改
             PrefabUtility.SaveAsPrefabAsset(prefabInstance, prefabPath);
             PrefabUtility.UnloadPrefabContents(prefabInstance);
-
-            Debug.Log("已成功移除 " + selectedObject.name + " 及其子物件的陰影，並保存到 Prefab。");
         }
     }
-
 }
