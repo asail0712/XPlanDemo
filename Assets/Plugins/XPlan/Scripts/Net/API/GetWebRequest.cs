5f4339afbda003560fc0e0063e7f3b985425aade
Ed Chen <ed@granden.rocks>
2025-07-09 18:38:02 +0800

    [Modify]Post API加上Query功能

--- Assets/Plugins/XPlan/Scripts/Net/API/GetWebRequest.cs
+++ Assets/Plugins/XPlan/Scripts/Net/API/GetWebRequest.cs
@@ -9,43 +9,10 @@ using XPlan.Utility;
 namespace XPlan.Net
 {
     public class GetWebRequest : WebRequestBase
-	{
-		private Dictionary<string, string> urlParams;
-	
+	{	
 		public GetWebRequest()
         {
-			urlParams = new Dictionary<string, string>();
-		}
-
-		protected void AddUrlParam(string key, string value)
-		{
-			value = UnityWebRequest.EscapeURL(value);
-
-			if (urlParams.ContainsKey(key))
-			{
-				urlParams[key] = value;
-			}
-			else
-			{
-				urlParams.Add(key, value);
-			}
-		}
-
-		override protected string GetUrl()
-		{
-			string url = base.GetUrl();
-
-			if (urlParams.Count > 0)
-			{
-				url += "?";
-
-				foreach (var urlParam in urlParams)
-				{
-					url += urlParam.Key + "=" + urlParam.Value;
-				}
-			}
-
-			return url;
+			
 		}
 
         override protected string GetRequestMethod()
--- Assets/Plugins/XPlan/Scripts/Net/API/WebRequestBase.cs
+++ Assets/Plugins/XPlan/Scripts/Net/API/WebRequestBase.cs
@@ -13,14 +13,16 @@ namespace XPlan.Net
     {
         private string apiUrl;
 		private Dictionary<string, string> headers;
-		private bool bWaitingNet;
+        private Dictionary<string, string> urlParams;
+        private bool bWaitingNet;
 		private bool bIgnoreError;
 		private int timeOut;
 
 		public WebRequestBase()
         {
 			headers			= new Dictionary<string, string>();
-			bWaitingNet		= true;
+            urlParams		= new Dictionary<string, string>();
+            bWaitingNet		= true;
 			bIgnoreError	= false;
 			timeOut			= 10;
         }
@@ -37,9 +39,40 @@ namespace XPlan.Net
             }
         }
 
+        public void AddUrlParam(string key, string value)
+        {
+            value = UnityWebRequest.EscapeURL(value);
+
+            if (urlParams.ContainsKey(key))
+            {
+                urlParams[key] = value;
+            }
+            else
+            {
+                urlParams.Add(key, value);
+            }
+        }
+
         public void SetUrl(string url)
         {
             this.apiUrl = url;
+        }
+
+        public string GetUrl()
+        {
+            string url = this.apiUrl;
+
+            if (urlParams.Count > 0)
+            {
+                url += "?";
+
+                foreach (var urlParam in urlParams)
+                {
+                    url += urlParam.Key + "=" + urlParam.Value;
+                }
+            }
+
+            return url;
         }
 
         public void SetWaiting(bool b)
@@ -132,11 +165,6 @@ namespace XPlan.Net
 		{
 			// nothing to do
 		}
-
-        virtual protected string GetUrl()
-        {
-			return apiUrl;
-        }
 
         virtual protected string GetRequestMethod()
         {
