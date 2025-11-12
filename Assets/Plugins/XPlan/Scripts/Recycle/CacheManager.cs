1a0bdcdfb8721379617c8fd00f229bc14ad7f184
asail0712 <asail0712@gmail.com>
2025-11-12 18:20:06 +0800

    [Add]加上Cache處理

---
+++ Assets/Plugins/XPlan/Scripts/Recycle/CacheManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace XPlan.Recycle
+{
+    public static class CacheManager
+    {
+        static ConcurrentDictionary<string, object> dataDict = new ConcurrentDictionary<string, object>();
+
+        static public bool SaveToCache<T>(string key, T data, bool bForce = false)
+        {
+            if(dataDict.ContainsKey(key) && !bForce)
+            {
+                return false;
+            }
+
+            dataDict[key] = data;
+
+            return true;
+        }
+
+        static public bool LoadFromCache<T>(string key, out T data)
+        {
+            if (!dataDict.ContainsKey(key))
+            {
+                data = default(T);
+                return false;
+            }
+
+            if (dataDict[key] is T)
+            {
+                data = (T)dataDict[key];
+                return true;
+            }
+            else
+            {
+                data = default(T);
+                return false;
+            }
+        }
+
+        static public void ClearCache()
+        {
+            dataDict.Clear();
+        }
+
+        static public void Remove(string key)
+        {
+            dataDict.Remove(key, out object dummy);
+        }
+    }
+}
--- Assets/Plugins/XPlan/Scripts/UI/UIBase.cs
+++ Assets/Plugins/XPlan/Scripts/UI/UIBase.cs
@@ -7,8 +7,7 @@ using UnityEngine.EventSystems;
 using UnityEngine.Networking;
 using UnityEngine.UI;
 
-using XPlan.Interface;
-using XPlan.Scenes;
+using XPlan.Recycle;
 using XPlan.UI.Fade;
 using XPlan.Utility;
 
@@ -410,62 +409,84 @@ namespace XPlan.UI
 			}));
 		}
 
-		public void LoadImageFromUrl(Image targetImage, string url, bool bResize = true)
-		{
-			if (string.IsNullOrEmpty(url))
-			{
-				LogSystem.Record("避免使用空字串下載圖片", LogType.Warning);
+        public void LoadImageFromUrl(Image targetImage, string url, bool bResize = true)
+        {
+            if (string.IsNullOrEmpty(url))
+            {
+                LogSystem.Record("避免使用空字串下載圖片", LogType.Warning);
 
-				return;
-			}
+                return;
+            }
 
-			MonoBehaviourHelper.StartCoroutine(LoadImageFromUrl_Internal(url, (texture) => 
-			{
-				Sprite sprite = Sprite.Create(
-									texture,
-									new Rect(0, 0, texture.width, texture.height),
-									new Vector2(0.5f, 0.5f));
+            MonoBehaviourHelper.StartCoroutine(LoadImageFromUrl_Internal(url, (texture) =>
+            {
+                if (texture == null)
+                {
+                    return;
+                }
 
-				targetImage.sprite = sprite;
+                // 建議：設定取樣
+                texture.filterMode = FilterMode.Bilinear; // 或 Point（像素風）
+                texture.wrapMode = TextureWrapMode.Clamp;
 
-				if (bResize)
-				{
-					// 自動調整 Image 尺寸符合原始圖片
-					RectTransform rt = targetImage.GetComponent<RectTransform>();
-					if (rt != null)
-					{
-						rt.sizeDelta = new Vector2(texture.width, texture.height);
-					}
-				}
-			}));
-		}
+                Sprite sprite = Sprite.Create(
+                                    texture,
+                                    new Rect(0, 0, texture.width, texture.height),
+                                    new Vector2(0.5f, 0.5f),
+                                    100f // pixelsPerUnit
+                                    );
 
-		private IEnumerator LoadImageFromUrl_Internal(string url, Action<Texture2D> finishAction)
-		{
-			UnityWebRequest request = UnityWebRequestTexture.GetTexture(url);
-			Texture2D texture		= null;
+                targetImage.sprite = sprite;
+                targetImage.preserveAspect = true;
 
-			yield return request.SendWebRequest();
+                if (bResize)
+                {
+                    // 自動調整 Image 尺寸符合原始圖片
+                    RectTransform rt = targetImage.GetComponent<RectTransform>();
+                    if (rt != null)
+                    {
+                        rt.sizeDelta = new Vector2(texture.width, texture.height);
+                    }
+                }
+            }));
+        }
+
+        private IEnumerator LoadImageFromUrl_Internal(string url, Action<Texture2D> finishAction)
+        {
+            Texture2D texture = null;
+
+            if (CacheManager.LoadFromCache<Texture2D>(url, out texture))
+            {
+                finishAction?.Invoke(texture);
+
+                yield break;
+            }
+
+            UnityWebRequest request = UnityWebRequestTexture.GetTexture(url);
+
+            yield return request.SendWebRequest();
 
 #if UNITY_2020_1_OR_NEWER
-			if (request.result != UnityWebRequest.Result.Success)
+            if (request.result != UnityWebRequest.Result.Success)
 #else
             if (request.isNetworkError || request.isHttpError)
 #endif
-			{
-				LogSystem.Record("載入圖片失敗: " + request.error, LogType.Error);
+            {
+                LogSystem.Record("載入圖片失敗: " + request.error, LogType.Error);
 
-				texture = null;
-			}
-			else
-			{
-				texture	= DownloadHandlerTexture.GetContent(request);
-			}
+                texture = null;
+            }
+            else
+            {
+                texture = DownloadHandlerTexture.GetContent(request);
 
-			finishAction?.Invoke(texture);
-		}
+                CacheManager.SaveToCache<Texture2D>(url, texture);
+            }
 
-		public void FadeInOutAlpha(CanvasGroup canvasGroup, float targetAlpha, float duration, Action finishAction = null)
+            finishAction?.Invoke(texture);
+        }
+
+        public void FadeInOutAlpha(CanvasGroup canvasGroup, float targetAlpha, float duration, Action finishAction = null)
 		{
 			MonoBehaviourHelper.StartCoroutine(FadeInOutAlpha_Internal(canvasGroup, targetAlpha, duration, finishAction));
 		}
