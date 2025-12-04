73e38c6706b9cef85fa69c0ae10bc0ebee717a9f
asail0712 <asail0712@gmail.com>
2025-12-04 17:27:01 +0800

    [Add]在MVVM架構下添加View取到VM的方式(只限XPlan內)

---
+++ Assets/Plugins/XPlan/Scripts/UI/Interface/IViewModelGetter.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace XPlan.UI
+{
+    internal interface IViewModelGetter<TViewModel> where TViewModel : ViewModelBase
+    {
+        void OnViewModelReady(TViewModel vm);
+    }
+}
