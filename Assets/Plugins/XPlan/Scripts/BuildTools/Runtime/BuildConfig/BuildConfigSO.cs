0877f98a9632817044310e67341d43464c2c469f
Your Daddy <>
2025-12-22 09:56:43 +0800

    [Modify]捕上傳

---
+++ Assets/Plugins/XPlan/Scripts/BuildTools/Runtime/BuildConfig/BuildConfigSO.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace XPlan.BuildTools.Runtime
+{
+    public abstract class BuildConfigSO : ScriptableObject
+    {
+        public string configId;
+        public virtual string DisplayName => string.IsNullOrEmpty(configId) ? name : $"{configId} ({name})";
+        public BuildConfigApplierSO applier;
+    }
+}
---
+++ Assets/Plugins/XPlan/Scripts/BuildTools/Runtime/BuildConfig/CurrentBuildConfig.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace XPlan.BuildTools.Runtime
+{
+    public class CurrentBuildConfig : ScriptableObject
+    {
+        public BuildConfigSO so;
+        public BuildConfigApplierSO applierSO;
+    }
+}
